cfa53a4a156c66ea106e081c4cee33413f0fcfde
小玉米 <xudan@MacBook-Pro-2.local>
2026-06-03 15:26:56 +0800

    Site updated: 2026-06-03 15:26:56

---
+++ js/newest-comments-data.js
@@ -0,0 +1,6 @@
+(function(){
+var d=[{"avatar":"https://avatars.githubusercontent.com/u/284277771?v=4","content":"#Test\ntest","nick":"xuyiming1216-cpu","url":"https://xuyiming1216-cpu.github.io/guestbook/index.html","date":"2026-06-03T07:10:18Z"}];
+if(!d.length)return;
+var e=Date.now()+0.006944444444444444*86400000;
+localStorage.setItem('github-newest-comments',JSON.stringify({value:d,expiry:e}));
+})();
